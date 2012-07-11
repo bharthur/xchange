61ee5da2106a511bdcb4b312974a072fc68b11ac
Ubuntu <ubuntu@ubuntu-desktop.(none)>
2012-07-12 02:48:06 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xchange::Application.config.secret_token = '51c5ce7c3683b785c30a14fe86067fb092d3fadec4f3786e152240d39f1457da7ce5e80203d63e7076fb1d0086a5b2cf072a03ead479cc04cfbcbbff80e6dca0'
